82175e36721cc2f87df614e3412f1f1bbbba75f4
Rashed Khan Aupurbo <69986425+rkaupurbo@users.noreply.github.com>
2020-10-29 20:24:11 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,48 @@
+$('#circle1').circleProgress({
+      value: 0.9,
+      size: 100,
+      fill: {
+        gradient: ["#30bae7", "#30bae7"]
+      }
+    });
+
+$('#circle2').circleProgress({
+      value: 0.75,
+      size: 100,
+      fill: {
+        gradient: ["#d74680", "#d74680"]
+      }
+    });
+$('#circle3').circleProgress({
+      value: 0.70,
+      size: 100,
+      fill: {
+        gradient: ["#15c7a8", "#15c7a8"]
+      }
+    });
+$('#circle4').circleProgress({
+      value: 0.85,
+      size: 100,
+      fill: {
+        gradient: ["#eb7d4b", "#eb7d4b"]
+      }
+    });
+
+
+
+
+
+//initiating jQuery
+              jQuery(function($) {
+                $(document).ready( function() {
+                  //enabling stickUp on the '.navbar-wrapper' class
+                  $('.navbar-wrapper').stickUp();
+                });
+              });
+
+
+
+        //initiating jQuery
+       
+            new WOW().init();
+        
